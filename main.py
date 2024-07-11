479daa63c1b04ddf296f26b05e75b568a44c28c9
ncmoliver <mrmarquesoliver@gmail.com>
2024-07-11 13:42:42 -0400

    timer formatted - break labels added with emojis

--- main.py
+++ main.py
@@ -6,15 +6,34 @@ GREY = "#758694"
 BEIGE = "#F7E7DC"
 VINTAGE = "#FFF8F3"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = .1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        label.config(text="Long Break 👏", fg=VINTAGE)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        label.config(text="Short Break 👍", fg=GREY)
+
+    else:
+        count_down(work_sec)
+        label.config(text="Work Time 🫡", fg=NAVY)
+
+    
+    count_down()
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
@@ -28,6 +47,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
     
 
 
